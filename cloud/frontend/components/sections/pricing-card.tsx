9858144f3f25f0dbcfa48411be32827a6b7b3612
Nicolas Oulianov <nicolas.oulianov@gmail.com>
2025-08-05 15:07:17 +0200

    Fix size

--- cloud/frontend/components/sections/pricing-card.tsx
+++ cloud/frontend/components/sections/pricing-card.tsx
@@ -9,7 +9,7 @@ interface PricingCardProps {
 export default function PricingCard({ userEmail }: PricingCardProps) {
   return (
     <div className="bg-white rounded-lg shadow-[0_10px_15px_rgba(0,0,0,0.1)] p-6 sm:p-8 max-w-sm mx-auto lg:mx-0 lg:ml-8">
-      <h2 className="text-2xl sm:text-2xl font-headline text-gray-900 mb-2 ">
+      <h2 className="text-2xl sm:text-4xl font-headline text-gray-900 mb-2 ">
         Get phospho pro now.
       </h2>
       <p className="text-sm sm:text-base text-gray-600 mb-6">
