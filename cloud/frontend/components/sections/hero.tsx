b3460fbb81eeaffa2eb3e9daa1878ccf4b907e1b
Nicolas Oulianov <nicolas.oulianov@gmail.com>
2025-08-05 15:06:34 +0200

    Improve style of the page

--- cloud/frontend/components/sections/hero.tsx
+++ cloud/frontend/components/sections/hero.tsx
@@ -1,11 +1,11 @@
 export default function HeroSection() {
-    return (
-        <section className="bg-gray-background py-4 px-4">
-            <div className="max-w-6xl mx-auto">
-                <h1 className="text-6xl font-headline text-dark-gray mb-16 max-w-4xl">
-                    Unlock the full power of phosphobot with phospho pro.
-                </h1>
-            </div>
-        </section>
-    )
+  return (
+    <section className="bg-gray-background py-2 px-4">
+      <div className="max-w-6xl mx-auto">
+        <h1 className="text-6xl font-headline text-dark-gray max-w-4xl mb-2">
+          Unlock the full power of phosphobot with phospho pro.
+        </h1>
+      </div>
+    </section>
+  );
 }
